dc385074272b01a85af8de23ab3c9899ef358166
Vinícius Yoda <vyoda4604@gmail.com>
2025-09-03 19:06:41 -0300

    rt

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -4,20 +4,20 @@ import { ITarefa, TarefasService } from '../../shared/services/api/tarefas/Taref
 import { ApiException } from '../../shared/services/api/ApiException';
 
 export const Dashboard = () => {
-    const [lista, setLista] = useState<ITarefa[]>([]);
+  const [lista, setLista] = useState<ITarefa[]>([]);
 
-    useEffect(() => {
-        TarefasService.getAll()
-            .then((result) => {
-                if (result instanceof ApiException) {
-                    alert(result.message)
-                } else {
-                    setLista(result);
-                }
-            })
-    }, []);
+  useEffect(() => {
+    TarefasService.getAll()
+      .then((result) => {
+        if (result instanceof ApiException) {
+          alert(result.message)
+        } else {
+          setLista(result);
+        }
+      })
+  }, []);
 
-    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
     if (e.key === 'Enter') {
       if (e.currentTarget.value.trim().length === 0) return;
 
@@ -38,7 +38,65 @@ export const Dashboard = () => {
     }
   }, [lista]);
 
-    return(
-        <div>Dashboard</div>
-    )
+  const handleToggleComplete = useCallback((id: number) => {
+    const tarefaToUpdate = lista.find((tarefa) => tarefa.id === id);
+    if (!tarefaToUpdate) return;
+
+    TarefasService.updateById(id, {
+      ...tarefaToUpdate,
+      isCompleted: !tarefaToUpdate.isCompleted,
+    })
+      .then((result) => {
+        if (result instanceof ApiException) {
+          alert(result.message);
+        } else {
+          setLista(oldLista => {
+            return oldLista.map(oldListItem => {
+              if (oldListItem.id === id) return result;
+              return oldListItem;
+            });
+          });
+        }
+      });
+  }, [lista]);
+
+  const handleDelete = useCallback((id: number) => {
+    TarefasService.deleteById(id)
+      .then((result) => {
+        if (result instanceof ApiException) {
+          alert(result.message);
+        } else {
+          setLista(oldLista => {
+            return oldLista.filter(oldListItem => oldListItem.id !== id);
+          });
+        }
+      });
+  }, []);
+
+  return (
+    <div>
+      <p>Lista</p>
+
+      <input onKeyDown={handleInputKeyDown} />
+
+      <p>{lista.filter((listItem) => listItem.isCompleted).length}</p>
+
+      <ul>
+        {lista.map((listItem) => {
+          return <li key={listItem.id}>
+            <input
+              type="checkbox"
+              checked={listItem.isCompleted}
+              onChange={() => handleToggleComplete(listItem.id)}
+            />
+
+            {listItem.title}
+
+            <button onClick={() => handleDelete(listItem.id)}>Apagar</button>
+          </li>;
+        })}
+      </ul>
+
+    </div>
+  );
 }
